db5d77e243e507167d597391a62cf5bc066944ae
carlalo4 <0619979792@uma.es>
2022-03-06 13:34:57 +0100

    First test and before and after added

--- src/test/java/org/carlalo4/fibonacci/FibonacciTest.java
+++ src/test/java/org/carlalo4/fibonacci/FibonacciTest.java
@@ -1,5 +1,39 @@
 package org.carlalo4.fibonacci;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test cases:
+ * fibonacci 0 -> 0
+ * fibonacci 1 -> 1
+ * fibonacci 2 -> 1
+ * fibonacci 3 -> 2
+ * fibonacci 4 -> 3
+ */
 public class FibonacciTest {
+    private Fibonacci fibonacci;
+
+    @BeforeEach
+    public void setup() {
+        fibonacci = new Fibonacci() ;
+    }
+
+    @AfterEach
+    public void finish() {
+        fibonacci = null ;
+    }
+
+    @Test
+    public void returnZeroIfTheNumberIsZero(){
+        int expectedValue = 0 ;
+        int obtainedValue = fibonacci.compute(0) ;
+
+        assertEquals(expectedValue, obtainedValue) ;
+    }
+
 
 }
